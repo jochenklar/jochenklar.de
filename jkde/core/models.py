780cdc569b44403a471b57695cd4780c90e9239d
Jochen Klar <jochenklar@gmail.com>
2019-01-02 12:07:11 +0100

    Add MainPage and TextPage

--- jkde/core/models.py
+++ jkde/core/models.py
@@ -36,3 +36,42 @@ class HomePage(SingletonMixin, Page):
         FieldPanel('body_de', classname='full'),
         ImageChooserPanel('image'),
     ]
+
+
+class MainPage(Page):
+
+    title_de = models.CharField(max_length=255, blank=True)
+
+    body = RichTextField(blank=True)
+    body_de = RichTextField(blank=True)
+
+    image = models.ForeignKey(
+        'wagtailimages.Image', blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
+
+    trans_title = TranslatedTitleField('title')
+    trans_body = TranslatedTextField('body')
+
+    content_panels = Page.content_panels + [
+        FieldPanel('title_de', classname="full title"),
+        FieldPanel('body', classname='full'),
+        FieldPanel('body_de', classname='full'),
+        ImageChooserPanel('image'),
+    ]
+
+
+class TextPage(Page):
+
+    title_de = models.CharField(max_length=255, null=False, blank=False)
+
+    body = RichTextField(blank=True)
+    body_de = RichTextField(blank=True)
+
+    trans_title = TranslatedTitleField('title')
+    trans_body = TranslatedTextField('body')
+
+    content_panels = [
+        FieldPanel('title', classname="full title"),
+        FieldPanel('title_de', classname="full title"),
+        FieldPanel('body', classname='full'),
+        FieldPanel('body_de', classname='full'),
+    ]
