75be5f188cf50e63813845a1e2928a5f995fa29a
Jochen Klar <jochenklar@gmail.com>
2019-01-15 14:09:23 +0100

    Add error templates

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.urls import include, path, re_path
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -26,3 +27,8 @@ if settings.DEBUG:
 
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler400 = TemplateView.as_view(template_name='core/400.html')
+handler403 = TemplateView.as_view(template_name='core/403.html')
+handler404 = TemplateView.as_view(template_name='core/404.html')
+handler500 = TemplateView.as_view(template_name='core/500.html')
